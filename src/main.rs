eb65fe8f32602364cf3139fe44ecfd093bc3e248
dotqurter <rigglemangamers@gmail.com>
2021-05-26 19:39:07 -0400

    create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use winit::{
+    event::{Event, WindowEvent}, 
+    event_loop::{ControlFlow, EventLoop}, 
+    window::WindowBuilder};
+
+
+struct Program {
+
+} 
+
+impl Program {
+    pub fn window_init() -> Self {
+
+        
+        Self {}
+    }
+}
+
+fn window_run() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).expect("Couldn't get WindowBuilder");
+
+    event_loop.run(move | event, _, control_flow | {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            }
+            if window_id == window.id() => *control_flow = ControlFlow::Exit,
+
+            _ => (),
+        }
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    window_run(); // ! This doesn't work as intended, should be by-reference
 }
